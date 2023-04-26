3a87e0acea2bd5b4cc767ac40d061d67700cfd16
Fredrick <mwangikingz94@gmail.com>
2023-04-27 00:46:55 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import GifListContainer from './GifListContainer';
 import NavBar from "./NavBar";
 
 // the App component should render out the GifListContainer component
